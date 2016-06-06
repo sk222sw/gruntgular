20fce8b1bc04df522dc506fa73eb3ab457caa23b
Sonny Kjellberg <sk222sw@student.lnu.se>
2016-06-06 20:23:46 +0000

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name btaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the btaApp
+ */
+angular.module('btaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name btaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the btaApp
+ */
+angular.module('btaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
